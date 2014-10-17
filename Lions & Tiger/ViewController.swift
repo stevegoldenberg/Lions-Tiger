ceb5fcfe244b1634e6a72b14d95256dcde5847bd
Steve Goldenberg <steve@stevegoldenberg.com>
2014-10-17 09:05:54 -0400

    Updating Mac Client

--- Lions & Tiger/ViewController.swift
+++ Lions & Tiger/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var myImageView: UIImageView!
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    @IBOutlet weak var breedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextButton(sender: UIBarButtonItem) {
+    }
 
+    
 }
 
---
+++ Tiger.swift
@@ -0,0 +1,18 @@
+//
+//  Tiger.swift
+//  Lions & Tiger
+//
+//  Created by Steve Goldenberg on 10/14/14.
+//  Copyright (c) 2014 Steve Goldenberg. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named: "")
+    
+}
